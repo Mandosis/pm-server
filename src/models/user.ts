7d528aa6d9ffe86b581017eb5f2d585acf8a9683
Chris Rabuse <rab233@live.com>
2016-10-18 18:19:40 -0500

    chore: add user model

---
+++ src/models/user.ts
@@ -0,0 +1,69 @@
+import * as mongoose from 'mongoose';
+import * as bcrypt from 'bcrypt';
+
+let Schema = mongoose.Schema;
+const SALT_WORK_FACTOR = 10;
+
+let userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    admin: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    joined: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+});
+
+userSchema.pre('save', function(next) {
+    let user = this;
+
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+        if (err) {
+            return next(err);
+        }
+
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) {
+                return next(err);
+            }
+
+            user.password = hash;
+
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword: string, done: any) {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+        if (err) {
+            return done(err);
+        } else {
+            done(null, isMatch);
+        }
+    });
+};
+
+let User = mongoose.model('User', userSchema);
+
+export { User };
